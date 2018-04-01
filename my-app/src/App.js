d3871840f3ed2b0fd667d69a7281601774b174ea
Preston McNabb <prestonmcnabb22@gmail.com>
2018-03-31 23:19:18 -0500

    tyring to fix stuff

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Clock from './components/Clock';
+import Header from './components/Header';
+import Display from './components/Display';
+
+export default class App extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            title: 'React Clock'
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <Header title={this.state.title} />
+                <Clock />
+            </div>
+        );
+    }
+};
